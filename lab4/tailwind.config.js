3c3ead5f579673549f53df1bb50ae92c7e83b258
Michał Świercz <loolok1232@gmail.com>
2025-11-15 15:58:23 +0100

    [LAB4][INIT]

---
+++ lab4/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          DEFAULT: '#6D28D9', 
+          light: '#8B5CF6',
+          dark: '#5B21B6',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
